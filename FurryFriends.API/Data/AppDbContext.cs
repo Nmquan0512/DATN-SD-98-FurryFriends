0405f422db249459144eacff76f0b31a714b024d
Nmquan0512 <93432250+Nmquan0512@users.noreply.github.com>
2025-06-05 04:40:51 +0700

    Models Nmquan 100%

--- FurryFriends.API/Data/AppDbContext.cs
+++ FurryFriends.API/Data/AppDbContext.cs
@@ -1,6 +1,39 @@
-﻿namespace FurryFriends.API.Data
+﻿using FurryFriends.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurryFriends.API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ChucVu> ChucVus { get; set; }
+        public DbSet<NhanVien> NhanViens { get; set; }
+        public DbSet<SanPham> SanPhams { get; set; }
+        public DbSet<GiamGia> GiamGias { get; set; }
+        public DbSet<DotGiamGiaSanPham> DotGiamGiaSanPhams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure relationships
+            modelBuilder.Entity<NhanVien>()
+                .HasOne(nv => nv.ChucVu)
+                .WithMany(cv => cv.NhanViens)
+                .HasForeignKey(nv => nv.ChucVuId);
+
+            modelBuilder.Entity<DotGiamGiaSanPham>()
+                .HasOne(d => d.GiamGia)
+                .WithMany(g => g.DotGiamGiaSanPhams)
+                .HasForeignKey(d => d.GiamGiaId);
+
+            modelBuilder.Entity<DotGiamGiaSanPham>()
+                .HasOne(d => d.SanPham)
+                .WithMany(s => s.DotGiamGiaSanPhams)
+                .HasForeignKey(d => d.SanPhamId);
+        }
     }
 }
--- FurryFriends.API/Models/SanPham.cs
+++ FurryFriends.API/Models/SanPham.cs
@@ -1,6 +1,20 @@
-﻿namespace FurryFriends.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FurryFriends.API.Models
 {
     public class SanPham
     {
+        [Key]
+        public Guid SanPhamId { get; set; }
+
+        [Required]
+        public string TenSanPham { get; set; }
+
+        public string? MoTa { get; set; }
+
+        public bool TrangThai { get; set; }
+
+        public virtual ICollection<DotGiamGiaSanPham> DotGiamGiaSanPhams { get; set; }
     }
+
 }
